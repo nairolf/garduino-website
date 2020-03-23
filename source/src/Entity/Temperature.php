a64c06e5fe7f2bd2e7e32ef657e5007b43c9f330
Florian Hanno <florian.hanno@twt.de>
2020-03-23 12:31:00 +0100

    added migrations bundle added temperature enity

---
+++ source/src/Entity/Temperature.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TemperatureRepository")
+ */
+class Temperature
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sensor;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $channel;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @return mixed
+     * @author Florian Hanno <florian.hanno@twt.de>
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSensor(): ?string
+    {
+        return $this->sensor;
+    }
+
+    public function setSensor(string $sensor): self
+    {
+        $this->sensor = $sensor;
+
+        return $this;
+    }
+
+    public function getChannel(): ?string
+    {
+        return $this->channel;
+    }
+
+    public function setChannel(string $channel): self
+    {
+        $this->channel = $channel;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
