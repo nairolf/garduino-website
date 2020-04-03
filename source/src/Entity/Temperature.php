25491c3bb71a57201683eb7d8b11b23d38b58d09
fhanno <fhanno@googlemail.com>
2020-04-03 09:04:38 +0200

    added validation and more formats

--- source/src/Entity/Temperature.php
+++ source/src/Entity/Temperature.php
@@ -2,17 +2,30 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Carbon\Carbon;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
- *     itemOperations={"get", "put"}
+ *     itemOperations={"get", "put"},
+ *     normalizationContext={"groups"={"temperature_listing:read"}, "swagger_definition_name"="Read"},
+ *     denormalizationContext={"groups"={"temperature_listing:write"}, "swagger_definition_name"="Write"},
+ *     attributes={
+ *          "pagination_items_per_page"=2,
+ *          "formats"={"jsonld", "json", "html", "jsonhal", "csv"={"text/csv"}}
+ *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\TemperatureRepository")
+ * @ApiFilter(SearchFilter::class, properties={"channel": "partial", "sensor": "partial"})
+ * @ApiFilter(RangeFilter::class, properties={"value"})
  */
 class Temperature
 {
@@ -25,29 +38,43 @@ class Temperature
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"temperature_listing:read", "temperature_listing:write"})
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     min="3",
+     *     max="256",
+     *     maxMessage="Name your sensor in max 256 chars."
+     * )
      */
     private $sensor;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"temperature_listing:read", "temperature_listing:write"})
+     * @Assert\NotBlank()
      */
     private $channel;
 
     /**
      * @ORM\Column(type="float")
+     * @Groups({"temperature_listing:read", "temperature_listing:write"})
+     * @Assert\NotBlank()
      */
     private $value;
 
     /**
+     * When was the temperature captured.
      * @ORM\Column(type="datetime")
+     * @Groups({"temperature_listing:read"})
      */
     private $createdAt;
 
     /**
-     * Set creation time to now()   
+     * Set creation time to now()
      */
-    public function __construct()
+    public function __construct(float $value = null)
     {
+        $this->value = $value;
         $this->createdAt = new DateTimeImmutable();
     }
 
@@ -87,13 +114,6 @@ class Temperature
     public function getValue(): ?float
     {
         return $this->value;
-    }
-
-    public function setValue(float $value): self
-    {
-        $this->value = $value;
-
-        return $this;
     }
 
     public function getCreatedAt(): ?DateTimeInterface
